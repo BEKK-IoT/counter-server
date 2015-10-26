ab5bf0d1c5a0acd776336254d0fba482345da8da
Vegard Veiset <veiset@gmail.com>
2015-10-26 16:45:56 +0100

    Update style.

--- counter.js
+++ counter.js
@@ -1,51 +1,50 @@
-    var Firebase = require('firebase');
-    var _ = require('lodash');
+const Firebase = require('firebase');
 
-    var firebase = new Firebase("https://fiery-inferno-7517.firebaseio.com/");
-    var users = [];
+const firebase = new Firebase("https://fiery-inferno-7517.firebaseio.com/");
+const users = [];
 
-    var stats = {
-        totalNumberOfEvents : -4,
-        totalNumberOfSpheroEvents : -1,
-        totalNumberOfGameEvents : -1,
-        totalNumberOfLampEvents : -1,
-        totalNumberOfUserEvents : -1
-    };
+const stats = {
+    totalNumberOfEvents: -4,
+    totalNumberOfSpheroEvents: -1,
+    totalNumberOfGameEvents: -1,
+    totalNumberOfLampEvents: -1,
+    totalNumberOfUserEvents: -1
+};
 
 
-    if (require.main === module) {
+if (require.main === module) {
 
-        console.log("Start");
+    console.log("Start");
 
-        firebase.child("gadgets/sphero").on("value", function(snapshot){
-            if(snapshot.val()){
-                stats.totalNumberOfSpheroEvents++;
-                stats.totalNumberOfEvents++;
-                firebase.child("stats").set(stats);
-            }
-        });
+    firebase.child("gadgets/sphero").on("value", function (snapshot) {
+        if (snapshot.val()) {
+            stats.totalNumberOfSpheroEvents++;
+            stats.totalNumberOfEvents++;
+            firebase.child("stats").set(stats);
+        }
+    });
 
-        firebase.child("gadgets/game").on("value", function(snapshot){
-            if(snapshot.val()){
-                stats.totalNumberOfGameEvents++;
-                stats.totalNumberOfEvents++;
-                firebase.child("stats").set(stats);
-            }
-        });
+    firebase.child("gadgets/game").on("value", function (snapshot) {
+        if (snapshot.val()) {
+            stats.totalNumberOfGameEvents++;
+            stats.totalNumberOfEvents++;
+            firebase.child("stats").set(stats);
+        }
+    });
 
-        firebase.child("gadgets/lamp").on("value", function(snapshot){
-            if(snapshot.val()){
-                stats.totalNumberOfLampEvents++;
-                stats.totalNumberOfEvents++;
-                firebase.child("stats").set(stats);
-            }
-        });
+    firebase.child("gadgets/lamp").on("value", function (snapshot) {
+        if (snapshot.val()) {
+            stats.totalNumberOfLampEvents++;
+            stats.totalNumberOfEvents++;
+            firebase.child("stats").set(stats);
+        }
+    });
 
-        firebase.child("users").on("value", function(snapshot){
-            if(snapshot.val()){
-                stats.totalNumberOfEvents++;
-                stats.totalNumberOfUserEvents++;
-                firebase.child("stats").set(stats);
-            }
-        });
-    }
+    firebase.child("users").on("value", function (snapshot) {
+        if (snapshot.val()) {
+            stats.totalNumberOfEvents++;
+            stats.totalNumberOfUserEvents++;
+            firebase.child("stats").set(stats);
+        }
+    });
+}
