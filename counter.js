8ca3f0c5e5c9c21369f268c2677b9c8f67f510ae
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2016-10-04 19:58:21 +0200

    added beacon data

--- counter.js
+++ counter.js
@@ -1,16 +1,16 @@
 const Firebase = require('firebase');
 
-const firebase = new Firebase("https://fiery-inferno-7517.firebaseio.com/");
+const firebase = new Firebase("https://beacon-wallboard.firebaseio.com/");
 const users = [];
 
-const stats = {
+let stats = {
     totalNumberOfEvents: -4,
     totalNumberOfSpheroEvents: -1,
     totalNumberOfGameEvents: -1,
     totalNumberOfLampEvents: -1,
-    totalNumberOfUserEvents: -1
+    totalNumberOfUserEvents: -1,
+    beacons: {}
 };
-
 
 if (require.main === module) {
 
@@ -47,4 +47,53 @@ if (require.main === module) {
             firebase.child("stats").set(stats);
         }
     });
+
+    firebase.child("users").on("value", function (snapshot) {
+        if (snapshot.val()) {
+          var obj = snapshot.val();
+          for(var name in obj) {
+              var team = name;
+              var registered = obj[name].registered;
+              var beacons = obj[name].beacons;
+              createBeaconStats(team, registered, beacons);
+          }
+        }
+    });
+}
+
+let beaconsMap = [
+  {name:"chocolateicecream", teams:new Map()},
+  {name:"mint", teams:new Map()},
+  {name:"blueberry", teams:new Map()},
+  {name:"ice", teams:new Map()},
+  {name:"mint1", teams:new Map()},
+  {name:"chocolateicecream1", teams:new Map()},
+  {name:"bubblegum", teams:new Map()},
+  {name:"merlot", teams:new Map()}
+]
+
+var createBeaconStats = function(team, registered, beacons) {
+  for(var minor in beacons) {
+    if(minor < beaconsMap.length && beacons[minor] > Date.now()-6000) {
+      beaconsMap[minor].teams.set(team, minor)
+    }
+  }
+}
+
+setInterval(function() {
+  sendBeaconStats();
+  whipeBeaconsMap();
+}, 3000);
+
+var sendBeaconStats = function() {
+  for(var beacon of beaconsMap) {
+    stats.beacons[beacon.name] = beacon.teams.size;
+  }
+  firebase.child("stats").set(stats);
+}
+
+var whipeBeaconsMap = function() {
+  for(var beacon of beaconsMap) {
+    beacon.teams = new Map();
+  }
 }
